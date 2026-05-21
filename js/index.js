a367d9d707b0d4ac39d2493cafa92b4ed3417a0e
Richard Lord <textstaras16@gmail.com>
2026-05-21 18:59:04 -0500

    js added, created footer and skills, readjusted flow layout, changed outlook

--- js/index.js
+++ js/index.js
@@ -6,3 +6,14 @@ const footer = document.createElement("footer");
 
 footer.textContent = `© ${thisYear} Richard Lord — All Rights Reserved`;
 body.appendChild(footer);
+
+// Create an array of your technical skills
+const skills = ["JavaScript", "HTML", "CSS", "Git", "Visual Studio Code", "PowerShell", "GitHub"];
+
+const skillsSection = document.querySelector("#Skills");
+const skillsList = skillsSection.querySelector("ul");
+for (let i = 0; i < skills.length; i++) {
+  const skill = document.createElement("li");
+    skill.innerText = skills[i];
+    skillsList.appendChild(skill);
+}
